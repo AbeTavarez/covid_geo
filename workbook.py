fe23070c6501d28735a49d342f14844763a7022c
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-06 23:29:33 -0500

    request

--- workbook.py
+++ workbook.py
@@ -6,3 +6,11 @@ import pandas as pd
 import folium
 from folium.plugis import HeatMap
 from pandas.io.json import json_normalize
+
+
+conn = http.client.HTTPSConnection("api.covid19api.com")
+payload = ''
+headers = {}
+conn.request("GET", "/summary", payload, headers)
+re = conn.getresponce()
+data = res.read().decode("UTF-8")
