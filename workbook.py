a60f859d836d2c910f56c2cbebce9eecc24f3ac7
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-06 23:51:14 -0500

    generating map and getting geo data

--- workbook.py
+++ workbook.py
@@ -17,3 +17,25 @@ data = res.read().decode("UTF-8")
 
 # Convert Data to JSON
 covid1 = json.loads(data)
+
+
+# Normalize data
+pd.json_normalize(covid1['Countries'], sep=",")
+
+# Convert to Pandas data frame
+df = pd.DataFrame(covid1['Countries'])
+# df
+
+# Drop column we don't need
+covid2 = df.drop(columns=['CountryCode', 'Slug', 'Date', 'Premium'], axis=1)
+# covid2
+
+# Generate base map
+map = folium.Map(tiles="Stamen Terrain", min_zoom=1.5)
+# map
+
+
+# gets geodata
+url = "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data"
+
+country_shapes = f'{url}/world-countries.json'
